ca770794a2bb0e05af9062e8e2921ea5edb5f500
Andre Coelho Carvalho <andre.acc@pf.gov.br>
2024-06-28 16:33:28 -0300

    versão final 1 28-06-2024

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function mostrarMenu(){
     if (containerMenuMobile.style.display == 'none'){
-        containerMenuMobile.style.display = 'block'
+        containerMenuMobile.style.display = 'block';
     }
     else{
         containerMenuMobile.style.display = 'none'
